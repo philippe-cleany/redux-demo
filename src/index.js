936143e21445592ff1fc5d948fb7f0d5f9c180c2
Philippe Saint Jacques <philippe@cleany.fr>
2017-10-18 11:13:09 +0200

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Reducers from './reducers'
+import App from './components/App'
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(Reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
